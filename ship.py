8f5ccf437a865b7ea1df2697be6df4b96ece4e4b
mugisha <mohamugi@gmail.com>
2025-03-14 15:02:51 +0300

    enable ship to move continuosly to the left

--- ship.py
+++ ship.py
@@ -16,11 +16,15 @@ class Ship:
 
         # movement flag
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """update the ship's position based on the movement flag"""
+        """update the ship's position based on the movement flags"""
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
+
 
     def blitme(self):
         """Draw the ship at its current location."""
