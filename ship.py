fc914d3ce729d5d6dd97c8903ee7c7f21621fd64
mugisha <mohamugi@gmail.com>
2025-03-17 12:45:13 +0300

    resize ship image

--- ship.py
+++ ship.py
@@ -6,29 +6,35 @@ class Ship:
         self.screen = screen
         self.ai_settings = ai_settings
 
-        # Load the ship image and get its rect
-        self.image = pygame.image.load("C:\\Users\\Administrator\\Desktop\\projects\\Alien_Invasion\\Alien_Invasion_game\\images\\ship.bmp")
+        # Load the ship image
+        self.original_image = pygame.image.load("C:\\Users\\Administrator\\Desktop\\projects\\Alien_Invasion\\Alien_Invasion_game\\images\\ship.bmp")
+        
+        # Resize the image
+        self.new_width = 100  # Set width
+        self.new_height = 100  # Set height
+        self.image = pygame.transform.scale(self.original_image, (self.new_width, self.new_height))
+        
+        # Get the rect of the resized image
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
         self.rect.bottom = self.screen_rect.bottom
 
-        # store a decimal value for the ship's center
+        # Store a decimal value for the ship's center
         self.center = float(self.rect.centerx)
         self.center_y = float(self.rect.centery)
 
-        # movement flag
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
 
     def update(self):
-        """update the ship's position based on the movement flags"""
-        # update the ship's center value, not the rect
+        """Update the ship's position based on the movement flags."""
+        # Update the ship's center value, not the rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
@@ -38,10 +44,9 @@ class Ship:
         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.center_y += self.ai_settings.ship_speed_factor
 
-        # update rect object from self.center
+        # Update rect object from self.center
         self.rect.centerx = self.center
         self.rect.centery = self.center_y
-
 
     def blitme(self):
         """Draw the ship at its current location."""
