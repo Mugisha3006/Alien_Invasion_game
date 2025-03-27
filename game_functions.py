761a1b2f4ff1f0f22b8e187ddbdc507e4542770d
mugisha <mohamugi@gmail.com>
2025-03-27 11:35:18 +0300

    create new fleet of aliens incase an alien hits the bottom of the screen

--- game_functions.py
+++ game_functions.py
@@ -148,6 +148,15 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
         # pause
         sleep(0.5)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """check if any aliens have reached the bottom of the screen"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # treat this the same as if the ship got hit
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
     
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if the fleet is at an edge, and then update the positions of all aliens in the fleet"""
@@ -157,6 +166,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # look for alien-ship collisions
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    
+    # look for aliens hitting the bottom of the screen
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 
    
